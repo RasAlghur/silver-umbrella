72d4890e6da7b4f5512f0110f7742c626d88e8d4
Theo <74920947+RasAlghur@users.noreply.github.com>
2024-11-15 10:13:47 +0100

    Update basicMintToken.sol

--- basicMintToken.sol
+++ basicMintToken.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
 
+import "@openzeppelin/contracts@5.1.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.1.0/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor(
+        address initialOwner,
+        string memory tokenName,
+        string memory tokenSymbol
+    ) ERC20(tokenName, tokenSymbol) Ownable(initialOwner) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        uint256 mintedAmount = amount * 10**decimals();
+        _mint(to, mintedAmount);
+    }
+}
